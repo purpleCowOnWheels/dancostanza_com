64d1166554a641733de848c8017d9840bb1577f5
purpleCowOnWheels <daniel.sky.costanza@gmail.com>
2017-11-06 13:43:54 -0500

    Adding initial versions of files

---
+++ app.js
@@ -0,0 +1,31 @@
+var main = function() {
+  /* Push the body and the nav over by 285px over */
+  $('.icon-menu').click(function() {
+    $('.menu').animate({
+      left: "0px"
+    }, 200);
+
+    $('body').animate({
+      left: "285px"
+    }, 200);
+  });
+
+  /* Then push them back */
+  $('.icon-close').click(function() {
+    $('.menu').animate({
+      left: "-285px"
+    }, 200);
+
+    $('body').animate({
+      left: "0px"
+    }, 200);
+  });
+  
+  /*toggle the sub-menu on pictures on and offs*/
+  $('#pictures').click(function(){
+    $('#picturesSubMenu').toggle(10);
+  });  
+};
+
+
+$(document).ready(main());
