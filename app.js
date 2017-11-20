28efba5424b8b24b38bdcb37678bbf6850516554
purpleCowOnWheels <daniel.sky.costanza@gmail.com>
2017-11-20 16:39:03 -0500

    Fixing bug caused by failed github upload

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var main = function() {
   /* Push the body and the nav over by 285px over */
   $('.icon-menu').click(function() {
@@ -30,37 +29,4 @@ var main = function() {
 };
 
 
-=======
-var main = function() {
-  /* Push the body and the nav over by 285px over */
-  $('.icon-menu').click(function() {
-    $('.menu').animate({
-      left: "0px"
-    }, 200);
-
-    $('body').animate({
-      left: "285px"
-    }, 200);
-  });
-
-  /* Then push them back */
-  $('.icon-close').click(function() {
-    $('.menu').animate({
-      left: "-285px"
-    }, 200);
-
-    $('body').animate({
-      left: "0px"
-    }, 200);
-  });
-  
-  /*toggle the sub-menu on pictures on and offs*/
-  $('#pictures').click(function(){
-//    alert( $('#picturesSubMenu').attr('id') )
-    $('#picturesSubMenu').toggle('hidden_custom');
-  });  
-};
-
-
->>>>>>> f971ba45093bcb87e72736f4217f23525c676740
 $(document).ready(main());
